cecbd7f9a23499b7f98f27c47f5cc90aacbfadb9
Jaewe Heo <jaeweheo@gmail.com>
2015-10-26 17:06:44 +0900

    :arrow_up: v0.2.0

---
+++ release.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+  version: '3.0.0',
+  debug: true,
+  protocol: 'https',
+  host: 'api.github.com',
+  timeout: 5000,
+  headers: {
+    'Accept': 'application/vnd.github.v3+json',
+    'user-agent': 'react-photonkit'
+  }
+});
+
+github.authenticate({
+  type: "oauth",
+  token: process.env.GITHUB_TOKEN
+});
+
+
+var owner = 'react-photonkit';
+var repo = 'react-photonkit';
+var version = '0.2.0';
+var note = 'create components (tab, table, and list)';
+
+github.releases.createRelease({
+  owner: owner,
+  repo: repo,
+  tag_name: version,
+  prerelease: true,
+  body: note
+});
