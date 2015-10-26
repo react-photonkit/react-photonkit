8066cf2bed594485953d075cf640e1375d9d9c90
Jaewe Heo <jaeweheo@gmail.com>
2015-10-26 22:26:34 +0900

    do not include react, react-dom in the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -10,5 +10,9 @@ module.exports = _.extend({}, baseConfig, {
     filename: 'photon.js',
     library: 'ReactPhotonKit',
     libraryTarget: 'umd'
-  }
+  },
+  externals: [
+    'react',
+    'react-dom'
+  ]
 });
