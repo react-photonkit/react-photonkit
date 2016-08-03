771ff4f566be6fe255c364dd7cf9fb71f4559d39
ragingwind <ragingwind@gmail.com>
2016-08-03 09:00:00 +0900

    Migrate2015: Table, Toolbar

---
+++ test/table-spec.jsx
@@ -0,0 +1,14 @@
+import expect from 'expect.js';
+import React from 'react';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
+import ReactDOM from 'react-dom';
+import Table from '../src/table.jsx';
+
+describe('table', () => {
+  it('Should create default element', () => {
+    const instance = ReactTestUtils.renderIntoDocument(<Table />);
+    const node = ReactDOM.findDOMNode(instance);
+
+    expect(node.className).to.be('table-striped');
+	});
+});
---
+++ test/toolbar-spec.jsx
@@ -0,0 +1,39 @@
+import expect from 'expect.js';
+import React from 'react';
+import ReactTestUtils from 'react/lib/ReactTestUtils';
+import ReactDOM from 'react-dom';
+import Toolbar from '../src/toolbar.jsx';
+
+describe('toolbar', () => {
+  it('Should create default element', () => {
+    const instance = ReactTestUtils.renderIntoDocument(<Toolbar />);
+		const toolbar = ReactDOM.findDOMNode(instance);
+    const classNames = toolbar.className.split(/\s+/);
+
+    expect(toolbar.nodeName).to.be('HEADER');
+    expect(classNames).to.contain('toolbar');
+    expect(classNames).to.contain('toolbar-header');
+	});
+
+
+  it('Should create toolbar(header) element', () => {
+    const instance = ReactTestUtils.renderIntoDocument(<Toolbar ptType="header" />);
+		const toolbar = ReactDOM.findDOMNode(instance);
+    const classNames = toolbar.className.split(/\s+/);
+
+    expect(toolbar.nodeName).to.be('HEADER');
+    expect(classNames).to.contain('toolbar');
+    expect(classNames).to.contain('toolbar-header');
+	});
+
+
+  it('Should create toolbar(footer) element', () => {
+    const instance = ReactTestUtils.renderIntoDocument(<Toolbar ptType="footer" />);
+		const toolbar = ReactDOM.findDOMNode(instance);
+    const classNames = toolbar.className.split(/\s+/);
+
+    expect(toolbar.nodeName).to.be('FOOTER');
+    expect(classNames).to.contain('toolbar');
+    expect(classNames).to.contain('toolbar-footer');
+	});
+});
