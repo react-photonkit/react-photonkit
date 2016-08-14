f784fc1cd3dfe2523d6240f1433dcd89dc63cc60
ragingwind <ragingwind@gmail.com>
2016-08-14 19:26:24 +0900

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
 'use strict';
 
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  module: {
-    preLoaders: [{
-      test: /\.js?/,
-      exclude: /node_modules/,
-      loader: 'xo-loader'
-    }],
-    loaders: [{
-      test: /\.js?/,
-      exclude: /node_modules/,
-      loaders: ['babel-loader']
-    }, {
-      test: /\.css$/,
-      loader: 'style-loader!css-loader'
-    }, {
-      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "url-loader?limit=1000000&mimetype=application/font-woff"
-    }, {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "file-loader"
-    }]
-  },
-  entry: path.resolve(__dirname, 'src/photon.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/',
-    filename: 'photon.js',
-    library: 'ReactPhotonKit',
-    libraryTarget: 'umd'
-  },
-  externals: [
-    'react',
-    'react-dom'
-  ],
-  xo: {
-    extends: ["xo", "xo-react"],
-    rules: {
-      "quote-props": ["error", "as-needed"]
-    }
-  }
+	module: {
+		preLoaders: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loader: 'xo-loader'
+		}],
+		loaders: [{
+			test: /\.jsx?$/,
+			exclude: /node_modules/,
+			loaders: ['babel-loader']
+		}, {
+			test: /\.css$/,
+			loader: 'style-loader!css-loader'
+		}, {
+			test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "url-loader?limit=1000000&mimetype=application/font-woff"
+		}, {
+			test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+			loader: "file-loader"
+		}]
+	},
+	entry: path.resolve(__dirname, 'src/photon.js'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: 'dist/',
+		filename: 'photon.js',
+		library: 'ReactPhotonKit',
+		libraryTarget: 'umd'
+	},
+	externals: [
+		'react',
+		'react-dom'
+	],
+	xo: {
+		extends: ["xo", "xo-react"],
+		rules: {
+			"quote-props": ["error", "as-needed"]
+		}
+	}
 };
